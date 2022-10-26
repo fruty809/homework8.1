da641a1c66ae3ee11efc71d30c886742f824ce7f
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 21:00:20 +0600

    Update RPG_Game.java

--- src/RPG_Game.java
+++ src/RPG_Game.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class RPG_Game {
+import java.util.Random;
+
+public class RPG_Game {
+    private static int roundNumber;
+    public static Random random = new Random();
+
+    public static void start() {
+        Boss boss = new Boss(1500, 50, "Dragon");
+        Warrior warrior = new Warrior(290, 10, "Golden Warrior");
+        Medic doc = new Medic(250, 5, 15, "Doctor psy");
+        Magic magic = new Magic(280, 20,5,"Dark Wizard");
+        Berserk berserk = new Berserk(270, 15, "Guts");
+        Medic junior = new Medic(260, 5, 5, "Psy assistant");
+        Thor thor = new Thor(200,15, "Ragnarok") ;
+        Hero[] heroes = {warrior, doc, magic, berserk, junior,thor,};
+
+
+        printStatistics(boss, heroes);
+        while (!isGameFinished(boss, heroes)) {
+            playRound(boss, heroes);
+        }
+    }
+
+    private static void playRound(Boss boss, Hero[] heroes) {
+        roundNumber++;
+        boss.chooseDefence(heroes);
+        boss.attack(heroes);
+        for (int i = 0; i < heroes.length; i++) {
+            if (heroes[i].getHealth() > 0 &&
+                    boss.getDefence() != heroes[i].getAbility()) {
+                heroes[i].attack(boss);
+                heroes[i].applySuperPower(boss, heroes);
+            }
+        }
+        printStatistics(boss, heroes);
+    }
+
+    private static boolean isGameFinished(Boss boss, Hero[] heroes) {
+        if (boss.getHealth() <= 0) {
+            System.out.println("Heroes won!!!");
+            return true;
+        }
+        boolean allHeroesDead = true;
+        for (int i = 0; i < heroes.length; i++) {
+            if (heroes[i].getHealth() > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("Boss won!!!");
+        }
+        return allHeroesDead;
+    }
+
+    private static void printStatistics(Boss boss, Hero[] heroes) {
+        System.out.println("ROUND " + roundNumber + " -----------");
+        System.out.println(boss);
+        for (int i = 0; i < heroes.length; i++) {
+            System.out.println(heroes[i]);
+        }
+    }
 }
