81410d8565021d3751cac032aee1cac801f087a1
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 20:55:30 +0600

    Update Berserk.java

--- src/Berserk.java
+++ src/Berserk.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Berserk {
+public class Berserk extends Hero {
+    private int savedDamage;
+
+    public Berserk(int health, int damage, String name) {
+        super(health, damage, SuperAbility.SAVE_DAMAGE_AND_REVERT, name);
+    }
+
+    public void setSavedDamage(int savedDamage) {
+        this.savedDamage = savedDamage;
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int getDamageOfBerserk = boss.getDamage() + this.getDamage();
+        boss.setHealth(boss.getHealth() - getDamageOfBerserk);
+        System.out.println("Berserk dealt blocked damage: " + getDamageOfBerserk);
+    }
+
 }
