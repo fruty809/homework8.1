5cdfa2225941a7b524bee76e082c90c3907b1746
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 20:58:20 +0600

    Update Magic.java

--- src/Magic.java
+++ src/Magic.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Magic {
+public class Magic extends Hero {
+    private int damagePlus;
+    public Magic(int health, int damage, int damagePlus, String name) {
+        super(health, damage, SuperAbility.BOOST, name);
+        this.damagePlus = damagePlus;
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        for (int i = 0; i < heroes.length; i++ ) {
+            if (heroes[i].getHealth() > 0 ) {
+                heroes[i].setDamage(heroes[i].getDamage() + this.damagePlus);
+
+            }
+        }
+    }
 }
