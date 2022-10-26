fbfc6561e1d6b6c2775881239eb1a52310a7b854
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 20:55:54 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Boss {
+public void chooseDefence(Hero[] heroes) {
+        int randomIndex = RPG_Game.random.nextInt(heroes.length);
+        this.defence = heroes[randomIndex].getAbility();
+    }
+
+    public void attack(Hero[] heroes) {
+        for (int i = 0; i < heroes.length; i++) {
+            if (heroes[i].getHealth() > 0) {
+                heroes[i].setHealth(heroes[i].getHealth() - this.getDamage());
+            }
+        }
+    }
+
+    public SuperAbility getDefence() {
+        return defence;
+    }
+
+    @Override
+    public String toString() {
+        return "BOSS " + super.toString() + " Defence: " +
+                (this.getDefence() == null ? "No defence" : this.getDefence());
+    }
 }
