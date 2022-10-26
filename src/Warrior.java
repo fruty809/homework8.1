be4012b5585ab63b143096e8664f89a4888af6c5
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 21:01:37 +0600

    Update Warrior.java

--- src/Warrior.java
+++ src/Warrior.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Warrior {
+public class Warrior extends Hero {
+    public Warrior(int health, int damage, String name) {
+        super(health, damage, SuperAbility.CRITICAL_DAMAGE, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int coefficient = RPG_Game.random.nextInt(5) + 2; // 2,3,4,5,6
+        boss.setHealth(boss.getHealth() - coefficient * this.getDamage());
+        System.out.println("Warrior hits critically " + coefficient * this.getDamage());
+    }
 }
