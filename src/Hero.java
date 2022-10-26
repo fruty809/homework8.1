1e8c89078c948535076546514c88ac1e93a8c378
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 20:57:50 +0600

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Hero {
+public abstract class Hero extends GameEntity
+        implements HavingSuperAbility {
+    private SuperAbility ability;
+
+    public Hero(int health, int damage, SuperAbility ability, String name) {
+        super(health, damage, name);
+        this.ability = ability;
+    }
+
+    public void attack(Boss boss) {
+        if (boss.getHealth() > 0) {
+            boss.setHealth(boss.getHealth() - this.getDamage());
+        }
+    }
+
+    public SuperAbility getAbility() {
+        return ability;
+    }
 }
