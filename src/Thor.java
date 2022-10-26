54c9c48fda0c7576db9be8825795f54725bf65b3
fruty809 <115156929+fruty809@users.noreply.github.com>
2022-10-26 21:01:20 +0600

    Update Thor.java

--- src/Thor.java
+++ src/Thor.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Thor {
+public class Thor  extends Hero{
+    boolean thorStan;
+    public Thor(int health, int damage,  String name) {
+        super(health, damage, SuperAbility.STAN, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        thorStan = RPG_Game.random.nextBoolean();
+        if (thorStan){
+            boss.setDamage(0);
+            System.out.println("Boss оглушен на 1 раунд");
+        }
+        else {
+            boss.setDamage(50);
+        }
+    }
 }
